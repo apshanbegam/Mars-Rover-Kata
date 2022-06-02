b0e376576d41187a576e61dddd4762687f74b613
apshanbegam <94931797+apshanbegam@users.noreply.github.com>
2022-06-02 21:09:25 +0100

    Initial Setup

---
+++ Mars-Rover-Kata/MarsRoverTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Mars_Rover_Kata;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
